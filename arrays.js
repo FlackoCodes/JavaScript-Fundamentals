2c9947831f30f0f3ffa14aa9621895325a7014a8
Flacko <lesliejoe2k2@gmail.com>
2024-04-11 21:34:01 +0000

    updated arrays

---
+++ arrays.js
@@ -0,0 +1,6 @@
+// arrays --> data structure used to store a collection of elements
+
+// nested arrays
+
+const array = [['Flacko','Tito'],['Frontend', 'Fullstack'], 'Engineer', 'Developer']
+console.log(array[0][1]);
