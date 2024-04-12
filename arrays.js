4ff98663a9e04e69854fdd9001d56082a06486a3
Flacko <lesliejoe2k2@gmail.com>
2024-04-12 21:07:48 +0000

    modified file

--- arrays.js
+++ arrays.js
@@ -3,4 +3,9 @@
 // nested arrays
 
 const array = [['Flacko','Tito'],['Frontend', 'Fullstack'], 'Engineer', 'Developer']
-console.log(array[0][1]);
+
+// getting elements from arrays using their indexes, arrays are 0 indexed
+
+console.log(array[0][1]); // selecting the second element in the fisrt array inside the 'array'
+
+//
