b702ee6fe66a68910cce490e0ae79fed5543d30a
Flacko <lesliejoe2k2@gmail.com>
2024-04-12 21:53:22 +0000

    modified file

--- arrays.js
+++ arrays.js
@@ -8,4 +8,16 @@ const array = [['Flacko','Tito'],['Frontend', 'Fullstack'], 'Engineer', 'Develop
 
 console.log(array[0][1]); // selecting the second element in the fisrt array inside the 'array'
 
-//
+// array methods
+
+// pop --> removes the last element in the array at index -1 or array.length -1 
+
+const poppedArray = ['fisrt', 'second', 'third']
+poppedArray.pop()
+console.log(poppedArray);
+
+// push --> appends an element to the array
+
+const appendedArray = ['Js', 'Python', 'Java', 'Ruby']
+appendedArray.push('Assembly')
+console.log(appendedArray);
