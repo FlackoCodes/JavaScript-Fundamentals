d35cae84a5101627878c8c8b80f124c2bfe33ca1
Flacko <lesliejoe2k2@gmail.com>
2024-04-17 04:25:46 +0100

    modified file

--- objects_destructuring.js
+++ objects_destructuring.js
@@ -12,3 +12,25 @@ console.log(fisrtName, lastName);
 
 const {fisrtName:fName, lastName:lName} = person
 console.log(fName, lName);
+
+
+// changing values
+
+let tools = {
+    frontend:"Json",
+    backend: "NodeJs", 
+    VersionControl: "mercurail"
+}
+
+let { tools:frontend = 'Html, css, Js', toools: VersionControl = 'git'} = tools;
+console.log(frontend, VersionControl);
+
+const person2 = {
+    fname : "Joe",
+    lname : 'Tito',
+    age : 26
+}
+
+const  {middlename = "Leslie", nextAge = 27} = person2 // adding new keys
+
+console.log(middlename)
